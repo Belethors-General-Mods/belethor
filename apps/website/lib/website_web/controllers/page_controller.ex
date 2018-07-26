e81eb0c277dea18feeb2cafc09084f33b04c7f89
Felix Krüger <fe.krueger@protonmail.com>
2018-07-26 15:12:46 +0200

    add /mods.html to render all mods, in a very alpha way

--- apps/website/lib/website_web/controllers/page_controller.ex
+++ apps/website/lib/website_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule WebsiteWeb.PageController do
   use WebsiteWeb, :controller
+  alias Website.{Repo, Mod}
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
   end
+
+  def mods(conn, _params) do
+    mods = Repo.all(Mod) |> Repo.preload(:tags)
+    render(conn, "mods.html", mods: mods)
+  end
+
 end
--- apps/website/lib/website_web/router.ex
+++ apps/website/lib/website_web/router.ex
@@ -17,6 +17,7 @@ defmodule WebsiteWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/mods.html", PageController, :mods
   end
 
   # Other scopes may use custom stacks.
