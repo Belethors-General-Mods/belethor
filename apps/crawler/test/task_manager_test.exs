71c137049487a64005433dec943ccc2036dc4325
Felix Krüger <fe.krueger@protonmail.com>
2018-10-29 18:52:09 +0100

    allow multiple task managers running at the same time

---
+++ apps/crawler/test/task_manager_test.exs
@@ -0,0 +1,15 @@
+defmodule TaskManagerTest do
+  use ExUnit.Case
+  require Logger
+
+
+  @tag capture_log: true
+  test "basic test ensure provider.search(query) gets called"
+
+  @tag capture_log: true
+  test "don't hang on unending tasks"
+
+  @tag capture_log: true
+  test "allow multiple managers running at the same time"
+
+end
