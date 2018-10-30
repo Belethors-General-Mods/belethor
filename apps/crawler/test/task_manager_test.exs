88acf0158a3f3d1c12e2d9785b07f2ecadd915f7
Felix Krüger <fe.krueger@protonmail.com>
2018-10-30 17:48:14 +0100

    implement first basic task_manager test

--- apps/crawler/test/task_manager_test.exs
+++ apps/crawler/test/task_manager_test.exs
@@ -2,9 +2,20 @@ defmodule TaskManagerTest do
   use ExUnit.Case
   require Logger
 
+  defmodule EchoProvider do
+    def search(query) do
+      [query]
+    end
+  end
+
 
   @tag capture_log: true
-  test "basic test ensure provider.search(query) gets called"
+  test "basic test ensure provider.search(query) gets called" do
+    input = "testvalue"
+    {:ok, mng} = Crawler.TaskManager.start_link(1)
+    {:ok, result} = Crawler.TaskManager.search(input, mng, EchoProvider)
+    assert result == [input]
+  end
 
   @tag capture_log: true
   test "don't hang on unending tasks"
