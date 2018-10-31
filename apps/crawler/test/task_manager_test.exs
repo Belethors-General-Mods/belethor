b140410ff2439eed4aaf221512c79753ab142167
Felix Krüger <fe.krueger@protonmail.com>
2018-10-31 13:26:08 +0100

    implement the test to don't hang on unending tasks
    it is strange that TaskManager already passes this test.

--- apps/crawler/test/task_manager_test.exs
+++ apps/crawler/test/task_manager_test.exs
@@ -7,6 +7,11 @@ defmodule TaskManagerTest do
       query
     end
   end
+
+  defmodule BlockingProvider do
+    def search(query) do
+      :timer.sleep(:infinite)
+      query
     end
   end
 
@@ -18,10 +23,28 @@ defmodule TaskManagerTest do
     assert result == input
   end
 
-  @tag capture_log: true
-  test "don't hang on unending tasks"
+  test "don't hang on unending tasks" do
+    {:ok, mng} = Crawler.TaskManager.start_link(2)
+    input = ["testvalue"]
 
-  @tag capture_log: true
+    block = fn ->
+      {exit_reason, _} = catch_exit(Crawler.TaskManager.search(3000, mng, BlockingProvider, 1000))
+      assert :timeout == exit_reason
+      :ok
+    end
+
+    valid = fn ->
+      Crawler.TaskManager.search(input, mng, EchoProvider)
+    end
+
+    queue = [block, block, block, valid]
+    expected = [:ok, :ok, :ok, input]
+    actual = Enum.map(queue, &Task.async/1) |> Enum.map(&Task.await/1)
+
+    assert expected == actual
+
+  end
+
   test "allow multiple managers running at the same time"
 
 end
