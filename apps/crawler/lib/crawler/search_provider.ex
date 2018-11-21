cc22774983d7941807164e6cfa447b4291cc4c68
Felix Krüger <fe.krueger@protonmail.com>
2018-11-21 15:04:10 +0100

    add a little documentation to search_provider

--- apps/crawler/lib/crawler/search_provider.ex
+++ apps/crawler/lib/crawler/search_provider.ex
@@ -1,4 +1,5 @@
 defmodule Crawler.SearchProvider do
+  @module_doc "beaviour to implement search providers for `Crawler.TaskManager`"
   @type search_result() :: list(any())
   @type query :: list(any())
   @callback search(query()) :: search_result()
