5af96b998a161c3b3c2c1096594830311f92454f
Felix Krüger <fe.krueger@protonmail.com>
2018-11-25 19:28:01 +0100

    start a task manager and its supervisor at crawler application start for each provider

--- apps/crawler/lib/crawler/application.ex
+++ apps/crawler/lib/crawler/application.ex
@@ -4,14 +4,56 @@ defmodule Crawler.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   def start(_type, _args) do
     # List all child processes to be supervised
-    children = []
+    children =
+      [
+        define_taskmanager(Steam, 3),
+        define_taskmanager(Nexus, 3),
+        define_taskmanager(Bethesda, 3)
+      ]
+      |> List.flatten()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Crawler.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  # defines a task manager at Crawler.#{name}.TaskManager
+  # and its supervisor at Crawler.#{name}.Supervisor
+  defp define_taskmanager(name, max_tasks) do
+    sup_name = Module.concat([Crawler, name, Supervisor])
+    man_name = Module.concat([Crawler, name, TaskManager])
+
+    [
+      %{
+        id: sup_name,
+        start:
+          {Task.Supervisor, :start_link,
+           [
+             [
+               strategy: :one_for_one,
+               restart: :transistent,
+               max_children: max_tasks,
+               max_restarts: 0
+             ]
+           ]}
+      },
+      %{
+        id: man_name,
+        start:
+          {Crawler.TaskManager, :start_link,
+           [
+             [
+               name: man_name,
+               max: max_tasks,
+               task_supervisor: sup_name
+             ]
+           ]}
+      }
+    ]
   end
 end
--- apps/crawler/test/crawler_test.exs
+++ apps/crawler/test/crawler_test.exs
@@ -1,4 +1,23 @@
 defmodule CrawlerTest do
   use ExUnit.Case
+  alias Crawler
+
   doctest Crawler
+
+  test "check if Crawler.Application has started its stuff" do
+    :ok = Application.ensure_started(:crawler)
+
+    assert match?(
+             [
+               {Crawler.Bethesda.TaskManager, _, :worker,
+                [Crawler.TaskManager]},
+               {Crawler.Bethesda.Supervisor, _, :worker, [Task.Supervisor]},
+               {Crawler.Nexus.TaskManager, _, :worker, [Crawler.TaskManager]},
+               {Crawler.Nexus.Supervisor, _, :worker, [Task.Supervisor]},
+               {Crawler.Steam.TaskManager, _, :worker, [Crawler.TaskManager]},
+               {Crawler.Steam.Supervisor, _, :worker, [Task.Supervisor]}
+             ],
+             Supervisor.which_children(Crawler.Supervisor)
+           )
+  end
 end
