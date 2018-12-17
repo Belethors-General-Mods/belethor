a4e9c381558b15a667d2afa10b82f4df04badfb6
iggi42 <38037032+iggi42@users.noreply.github.com>
2018-12-17 03:27:46 +0100

    improve documentation docs

--- apps/crawler/lib/crawler/client_behaviour.ex
+++ apps/crawler/lib/crawler/client_behaviour.ex
@@ -1,12 +1,20 @@
 defmodule Crawler.Client do
   @moduledoc """
-  beaviour to implement search providers for `Crawler.TaskManager`
+  This module describes client behaviour to be used by `Crawler.TaskManager.search/4`.
 
-  the implementations are meant to download and parse
+  The only function to be implemented is the `search/1` function.
   """
   @type result() :: any()
   @type search_result() :: {:ok, [mod()]} | {:error, term()}
   @type query :: list(any())
+  @doc """
+  This function is meant to query an provider.
+  In the usual version of an http based api from the provider, you need to do:
+  Query the provider server, with the argumens of in the `query()`.
+  Then parse the data into `[result()]`.
+  If all this worked as expected return {:ok, [result()]}.
+  If not return {:error, reason :: term()}.
+  """
   @callback search(query()) :: search_result()
 
   # TODO :)
