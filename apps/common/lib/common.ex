fbe755227b9529c6f90b501a65eac711312381c9
iggi <iggi@m3lt.work>
2018-12-18 14:25:08 +0100

    remove hello world function

--- apps/common/lib/common.ex
+++ apps/common/lib/common.ex
@@ -2,17 +2,4 @@ defmodule Common do
   @moduledoc """
   Documentation for Common.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Common.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/common/test/common_test.exs
+++ apps/common/test/common_test.exs
@@ -1,8 +1,4 @@
 defmodule CommonTest do
   use ExUnit.Case
   doctest Common
-
-  test "greets the world" do
-    assert Common.hello() == :world
-  end
 end
