b9085117f4d4f6cc00f19cf54942e7f07077b9cc
skwerlman <skwerlman@users.noreply.github.com>
2018-12-17 20:41:27 -0500

    drop support for ex 1.6 and otp 20

--- apps/database/mix.exs
+++ apps/database/mix.exs
@@ -9,7 +9,7 @@ defmodule Database.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
