96a2e55411d92474e0e27057eaf4e83d939ce203
skwerlman <skwerlman@users.noreply.github.com>
2019-02-24 01:13:56 -0500

    test error helpers

--- apps/tag_editor/mix.exs
+++ apps/tag_editor/mix.exs
@@ -45,7 +45,8 @@ defmodule TagEditor.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:stream_data, "~> 0.4", only: :test}
     ]
   end
 end
---
+++ apps/tag_editor/test/tag_editor_web/views/error_helpers_test.exs
@@ -0,0 +1,39 @@
+defmodule TagEditorWeb.ErrorHelpersTest do
+  @moduledoc false
+  use TagEditorWeb.ConnCase, async: true
+  use ExUnitProperties, async: true
+  import StreamData
+  alias TagEditorWeb.ErrorHelpers
+
+  @max_run_time Application.get_env(:ex_unit, :test_timings, [max_run_time: 500])[:max_run_time]
+  @max_runs Application.get_env(:ex_unit, :test_timings, [max_runs: 1_000_000_000])[:max_runs]
+
+  test "translates known errors" do
+    # assert TagEditorWeb.ErrorHelpers.translate_error({"some known message", []})
+  end
+
+  property "leaves unknown errors alone" do
+    check all message <- string(:printable) do
+      t_msg = ErrorHelpers.translate_error({"test " <> message, []})
+      assert t_msg == "test " <> message
+    end
+  end
+
+  property "translates plurals correctly" do
+    check all count <- one_of([constant(0), positive_integer()]),
+              max_run_time: @max_run_time,
+              max_runs: @max_runs do
+      t_msg = ErrorHelpers.translate_error({"test %{count}", [count: count]})
+      assert t_msg == "test " <> Integer.to_string(count)
+    end
+
+    # negative integers should always raise
+    check all invalid_count <- positive_integer() |> map(fn x -> x * -1 end),
+              max_run_time: @max_run_time,
+              max_runs: @max_runs do
+      assert_raise FunctionClauseError, fn ->
+        ErrorHelpers.translate_error({"test %{count}", [count: invalid_count]})
+      end
+    end
+  end
+end
