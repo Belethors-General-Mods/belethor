b64e27894736c2004fad9ca7dda785a7ee8d62c4
Ignatz <iggi@m3lt.work>
2019-05-10 14:33:54 +0200

    config tag_editor on port 4040 in dev

--- apps/tag_editor/config/dev.exs
+++ apps/tag_editor/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :tag_editor, TagEditorWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4040],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
