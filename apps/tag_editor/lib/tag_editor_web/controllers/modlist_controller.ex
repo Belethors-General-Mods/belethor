08b1cddba8b459d0831bf47b74808025a75fd422
Ignatz <iggi@m3lt.work>
2019-05-13 09:26:11 +0200

    setup up the controller for modlists

---
+++ apps/tag_editor/lib/tag_editor_web/controllers/modlist_controller.ex
@@ -0,0 +1,24 @@
+defmodule TagEditorWeb.ModListController do
+  use TagEditorWeb, :controller
+
+  # lots of TODO things
+  def index(conn, %{ "id" => _id}) do
+    render(conn, "index.html")
+  end
+
+  def new(conn, %{ "id" => _id }) do
+    render(conn, "index.html")
+  end
+
+  def create(conn, %{ "id" => _id }) do
+    render(conn, "index.html")
+  end
+
+ def update(conn, %{ "id" => _id }) do
+    render(conn, "index.html")
+  end
+
+  def delete(conn, %{ "id" => _id}) do
+    render(conn, "index.html")
+  end
+end
