eb24f99678bb0fcfd4e70f591c1f030b457e5719
Ignatz <iggi@m3lt.work>
2019-05-13 13:50:46 +0200

    add modlists to the data model

---
+++ apps/common/lib/struct/modlist.ex
@@ -0,0 +1,28 @@
+defmodule Common.Struct.ModList do
+  @moduledoc """
+  Represents a mod.
+  """
+  alias Common.Struct.ModList
+
+  @enforce_keys [:name, :mods]
+  defstruct [
+    :name,
+    :description,
+    :mods
+  ]
+
+  @typedoc "typespec for a mod"
+  @type t :: %ModList{}
+
+  def validate(%ModList{name: nil}) do
+    {:error, :empty}
+  end
+
+  def validate(%ModList{mods: nil}) do
+    {:error, :empty}
+  end
+
+  def validate(%ModList{}) do
+    :ok
+  end
+end
