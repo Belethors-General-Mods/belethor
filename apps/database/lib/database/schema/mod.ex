c744f1a96d907cfa653c4942bcf209bb1e644693
Ignatz <iggi@m3lt.work>
2019-05-15 11:16:18 +0200

    add schema <-> struct transformation and make ModFile naming consistent.

--- apps/database/lib/database/schema/mod.ex
+++ apps/database/lib/database/schema/mod.ex
@@ -4,14 +4,41 @@ defmodule Database.Schema.Mod do
   """
   use Ecto.Schema
 
+  alias Database.Repo
+  alias Database.Schema
+  alias Common.Struct
+
   schema "mod" do
     field(:name, :string)
     field(:desc, :string)
     field(:published, :boolean)
     field(:image, :string)
 
-    embeds_one(:oldrim, Database.Schema.ModFile)
-    embeds_one(:sse, Database.Schema.ModFile)
-    many_to_many(:tags, Database.Schema.ModTag, join_through: "mods_tags", unique: true)
+    embeds_one(:oldrim, Schema.ModFile)
+    embeds_one(:sse, Schema.ModFile)
+    many_to_many(:tags, Schema.ModTag, join_through: "mods_tags", unique: true)
+  end
+
+  def struct2schema(%Struct.Mod{} = struct) do
+    %__MODULE__ {
+      name: struct.name,
+      desc: struct.description,
+      published: struct.published,
+      sse: Schema.ModFile.from_struct(struct.sse),
+      oldrim: Schema.ModFile.from_struct(struct.oldrim),
+      tags: Enum.map(struct.tags, &Schema.ModTag.from_struct/1)
+    }
+  end
+
+  def schema2struct(%__MODULE__{} = schema) do
+    schema = Repo.preload(schema, [:tags])
+    %Struct.Mod {
+      name: schema.name,
+      description: schema.desc,
+      published: schema.published,
+      sse: Schema.ModFile.to_struct(schema.sse),
+      oldrim: Schema.ModFile.to_struct(schema.oldrim),
+      tags: Enum.map(schema.tags, &Schema.ModTag.to_struct/1)
+    }
   end
 end
--- apps/database/lib/database/schema/mod_tag.ex
+++ apps/database/lib/database/schema/mod_tag.ex
@@ -4,8 +4,19 @@ defmodule Database.Schema.ModTag do
   """
   use Ecto.Schema
 
+  alias Database.Schema
+  alias Common.Struct
+
   schema "mod_tag" do
     field(:name, :string)
     many_to_many(:mods, Database.Schema.Mod, join_through: "mods_tags", unique: true)
+  end
+
+  def struct2schema(string) when is_bitstring(string) do
+    %Schema.ModTag{ name: string }
+  end
+
+  def schema2struct(%Schema.ModTag{} = schema) do
+    schema.name
   end
 end
